09fd1056f12a04afa5b53034eb08288305b4ab1c
Murray Hoggett <murrayhoggett@gmail.com>
2017-07-29 10:58:39 +0100

    Got basemap plotting working. Basemap has some bugs and therefore won't download the etopo or satallite data :(

--- gpsCAT.py
+++ gpsCAT.py
@@ -6,9 +6,12 @@ gpsCat
 
 """
 
-import gpxpy 
+import gpxpy
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import mplleaflet
+from mpl_toolkits.basemap import Basemap
 
 gpx_file = open('170507.GPX', 'r')
 gpx = gpxpy.parse(gpx_file)
@@ -21,9 +24,37 @@ for track in gpx.tracks:
 
 cat_track = pd.DataFrame(track_coords, columns=['Latitude','Longitude','Elevation','Time'])
 
-#plt.plot(cat_track['Longitude'],cat_track['Latitude'])
-#plt.show()
+## Find the smallest and greatest coordinates, for plotting
+maxlat = np.max(cat_track.Latitude)
+maxlong = np.max(cat_track.Longitude)
+minlat = np.min(cat_track.Latitude)
+minlong = np.min(cat_track.Longitude)
 
-plt.plot(cat_track['Time'], cat_track['Elevation'])
+print(maxlat, maxlong, minlat, minlong)
+
+## Standard plot with no background
+# plt.plot(cat_track['Longitude'],cat_track['Latitude'])
+# plt.show()
+
+## Can't get this working yet...
+# cat_track = cat_track.dropna()
+# fig = plt.figure(1)
+# plt.plot(cat_track['Longitude'], cat_track['Latitude'], color='darkorange', linewidth=5, alpha=0.5)
+# mplleaflet.display(fig=fig, tiles='esri_aerial')
+# plt.show()
+
+#
+m = Basemap(projection='merc', lat_0 = ((minlat + maxlat)/2.0), lon_0 = ((minlong + maxlong)/2.0),
+            llcrnrlat = minlat,
+            llcrnrlon = maxlong,
+            urcrnrlat = maxlat,
+            urcrnrlon = minlong,
+            resolution='l', epsg=4326)
+m.fillcontinents(color='coral',lake_color='aqua')
+## Note this is an infuriating bug! must have cat_track.Latitude.values. If you don't, the latlon keyword causes random crashes.
+m.plot(cat_track.Longitude.values, cat_track.Latitude.values, linewidth=1.5, color='r', latlon=True)
+plt.title("catGPS track")
 plt.show()
 
+# plt.plot(cat_track['Time'], cat_track['Elevation'])
+# plt.show()
