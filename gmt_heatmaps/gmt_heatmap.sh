01c8dd399900d656b487713738cf500d0f8907c5
Murray Hoggett <murrayhoggett@gmail.com>
2017-08-20 21:10:44 +0100

    Added colourbar, map scale, sorted axes, etc. Final things to do - add background image

--- gmt_heatmaps/gmt_heatmap.sh
+++ gmt_heatmaps/gmt_heatmap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+gmtset PS_MEDIA A3
+
 ## Make file list to loop over, read gpx files with python and dump to csv
 # files=`ls *GPX`
 # rm all_gpx_files.csv
@@ -19,12 +21,18 @@ precision=0.00005
 rgn=`gmtinfo -I0.0001 all_gpx_files.txt`
 prj="-JM6i"
 outfile="heatmap.ps"
+grdfile="heatmap.nc"
+scalelat=`gmtinfo all_gpx_files.txt -I0.001 | awk 'BEGIN{FS="/"}{print $3}'`
+
 # Make grid of gps counts in bins
 blockmean all_gpx_files.txt -Sn $rgn -I$precision > grid_counts.txt
-nearneighbor grid_counts.txt $rgn -I$precision -Gheatmap.nc -S20e -N4
-psxy $rgn $prj grid_counts.txt -P -B0.01 -SC0.02 -K > $outfile
-grdimage $rgn $prj heatmap.nc  -B0.01 -O -t50 >> $outfile
+nearneighbor grid_counts.txt $rgn -I$precision -G${grdfile} -S20e -N4
+makecpt -Chot `grdinfo ${grdfile} -T0.1` > heatmap.cpt
+psxy $rgn $prj grid_counts.txt -P -SC0.02 -K > $outfile
+pscoast $rgn $prj -Ia -Lx5.25i/0.5i+c${scalelat}+w100e -K -O >> $outfile
+grdimage $rgn $prj $grdfile -Cheatmap.cpt -B0.001SWne -t50 -K -O >> $outfile
+psscale -Cheatmap.cpt -D6.5i/3i+w4i/0.25i -B10 -O >> $outfile
 ps2pdf $outfile heatmap.pdf
 evince heatmap.pdf
 
-## This now works. TODO - add scale, colorbar, borders, title, and background image. 
+## This now works. TODO - add background image.
